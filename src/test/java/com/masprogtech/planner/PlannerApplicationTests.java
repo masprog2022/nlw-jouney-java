6647924ecf75fd69804aceb97e199e3d35ca5560
masprog2022 <masprog2022@gmail.com>
2024-07-09 22:27:50 +0100

    config: initial commit

---
+++ src/test/java/com/masprogtech/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masprogtech.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
